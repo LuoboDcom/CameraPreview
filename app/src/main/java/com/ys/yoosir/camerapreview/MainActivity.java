1da8b4adb9cf7fb7fbb26df0168666285b6914e3
yueshan <yueshan0607@163.com>
2016-08-24 20:01:17 +0800

    first create

--- app/src/main/java/com/ys/yoosir/camerapreview/MainActivity.java
+++ app/src/main/java/com/ys/yoosir/camerapreview/MainActivity.java
@@ -2,6 +2,8 @@ package com.ys.yoosir.camerapreview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SurfaceView mSurfaceView = (SurfaceView) findViewById(R.id.surface_view);
+        mSurfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+            }
+        });
     }
 }
